b840e9f87f2312cab8858a9cc3684231fcbb8ec4
Donnie Nowels <djnowels11@gmail.com>
2016-11-05 11:23:07 -0400

    added help steps in orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,6 @@
-require("./connection.js")
+var connection = require("./connection.js")
+
+//ORM example
 
 var selectAll = function {
 
@@ -27,3 +29,31 @@ connection.query('UPDATE burgers SET burger_name, 1 = ? WHERE id = ?', [req.body
 };
 
 module.exports = orm.js
+
+var connection = require('./connection.js');
+
+// object relational mapper (ORM)
+
+var orm = {
+  selectWhere: function (tableInput, colToSearch, valOfCol) {
+    var queryString = 'SELECT * FROM ' + tableInput + ' WHERE ' + colToSearch + ' = ?';
+    connection.query(queryString, [valOfCol], function (err, result) {
+      console.log(result);
+    });
+  },
+  selectAndOrder: function (whatToSelect, table, orderCol, orderBy) {
+    var queryString = 'SELECT ' + whatToSelect + ' FROM ' + table + ' ORDER BY ' + orderCol + ' ' + orderBy;
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      console.log(result);
+    });
+  },
+  findWhoHasMost: function (tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+    var queryString = 'SELECT ' + tableOneCol + ', COUNT(' + tableOneCol + ') AS count FROM ' + tableOne + ' LEFT JOIN ' + tableTwo + ' ON ' + tableTwo + '.' + tableTwoForeignKey + '= ' + tableOne + '.id GROUP BY ' + tableOneCol + ' ORDER BY count desc LIMIT 1';
+    connection.query(queryString, function (err, result) {
+      console.log(result);
+    });
+  }
+};
+
+module.exports = orm;
