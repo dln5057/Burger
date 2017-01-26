17d73e7eab6326060a4246b75716db0306651dcb
Donnie Nowels <djnowels11@gmail.com>
2017-01-26 18:06:38 -0500

    heroku problems

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else{
-  connection = mysql.createConnection({
+  connection = mysql.createConnection({ 
     host     : 'localhost',
     user     : 'root',
     password : 'password', // Add your password
--- models/burger.js
+++ models/burger.js
@@ -4,7 +4,7 @@ var burger = {
 	all: function(cb) {
 		orm.all('burgers', function(res){
 			cb(res)
-		});
+		}); 
 	},
 	create: function(name,cb) {
 		orm.create('burgers', ['burger_name', 'devoured'], [name, false], cb);
